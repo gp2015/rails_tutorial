0bf9b6cfdd8c269961c6a646f25ca87f3882c458
gp2015 <georgec.perez@gmail.com>
2017-04-04 19:53:09 -0400

    Updated routes and static_pages/*

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters" #adds green/red to minitest
+Minitest::Reporters.use! #adds green/red to minitest
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
