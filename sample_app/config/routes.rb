9c46b16abb69fc0691937547a5590281687ce673
gp2015 <georgec.perez@gmail.com>
2017-04-15 16:09:10 -0400

    Worked through lesson 5

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  
+
   #root 'application#home'
 
-  root 'static_pages#home'
+  root 'static_pages#home' # get '/' => 'static_pages#home'
+  # get '/home' => 'statc_pages#home'
+  # get 'static_pages/help'
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get '/login' => 'static_pages#login'
+  # get 'users/new'
+  get '/signup' => 'users#new'
 
   # get 'static_pages/home'
 
---
+++ sample_app/test/controllers/users_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+
+  extended_title = " | Ruby on Rails Tutorial Sample App"
+
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_select "title", "Sign up" + extended_title
+  end
+
+end
