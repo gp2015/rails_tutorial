3f14ae47341f23c2b8c80740dc4a61997c79556f
gp2015 <georgec.perez@gmail.com>
2017-04-15 23:00:01 -0400

    Added comments to unit tests

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -2,29 +2,29 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-  extended_title = " | Ruby on Rails Tutorial Sample App"
+  extended_title = "| Ruby on Rails Tutorial Sample App"
 
   test "should get home" do
-    get :home
+    get :home # vs get_static_pages_home_url
     assert_response :success
-    assert_select "title", "Home" + extended_title
+    assert_select "title", "Home #{extended_title}"
   end
 
   test "should get help" do
-    get :help
+    get :help # vs get_static_pages_help_url
     assert_response :success
-    assert_select "title", "Help" + extended_title
+    assert_select "title", "Help #{extended_title}"
   end
 
   test "should get about" do
-    get :about
+    get :about # vs get_static_pages_about_url
     assert_response :success
-    assert_select "title", "About" + extended_title
+    assert_select "title", "About #{extended_title}"
   end
 
   test "should get contact" do
-    get :contact
+    get :contact # vs get_static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact" + extended_title
+    assert_select "title", "Contact #{extended_title}"
   end
 end
--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
-  extended_title = " | Ruby on Rails Tutorial Sample App"
+  extended_title = "| Ruby on Rails Tutorial Sample App"
 
   test "should get new" do
     get :new
     assert_response :success
-    assert_select "title", "Sign up" + extended_title
+    assert_select "title", "Sign up #{extended_title}"
   end
 
 end
